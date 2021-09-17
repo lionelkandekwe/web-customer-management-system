5ed37604eddd08694d5a06d5f14697a35bffacbb
lionelkandekwe <liokandekwe@gmail.com>
2021-09-17 10:51:51 +0200

    Create Customer class & set up customerDao

---
+++ src/main/java/com/webcustomer/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,12 @@
+package com.webcustomer.springdemo.dao;
+
+import java.util.List;
+
+import com.webcustomer.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	
+
+	public List<Customer>getCustomers();
+}
---
+++ src/main/java/com/webcustomer/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,46 @@
+package com.webcustomer.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.webcustomer.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	//Inject session Factory
+	
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		
+		//get the current hibernate session
+		
+		Session currentSession=sessionFactory.getCurrentSession();
+		
+		// create a query
+		
+		Query<Customer> theQuery=currentSession.createQuery("from Customer",Customer.class);
+				
+		// execute query and get result list
+		
+		List<Customer> customers=theQuery.getResultList();
+		
+		// return the results
+		
+		return customers;
+	}
+
+}
