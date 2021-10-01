994a29a7d158c3c39b6d7c108cd1a725e6278950
lionelkandekwe <liokandekwe@gmail.com>
2021-10-01 18:49:43 +0200

    Add @BeforeAdvice & @AfterReturningAdvice

--- src/main/java/com/webcustomer/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/webcustomer/springdemo/aspect/CRMLoggingAspect.java
@@ -2,9 +2,13 @@ package com.webcustomer.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
 
 
 
@@ -17,13 +21,13 @@ public class CRMLoggingAspect {
 	private Logger myLogger=Logger.getLogger(getClass().getName());
 	
 	//setup pointcut declarations
-	@Pointcut("execution(* com.webcustomer.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.webcustomer.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.webcustomer.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.webcustomer.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.webcustomer.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.webcustomer.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
@@ -31,6 +35,44 @@ public class CRMLoggingAspect {
 	
 	//add @Before advice
 	
+	@Before("forAppFlow()")
+	public void beforeAdvice(JoinPoint theJoinPoint) {
+		
+		// display method
+		
+		String theMethod=theJoinPoint.getSignature().toShortString();
+		myLogger.info("====> In @Before: calling method "+theMethod);
+		
+		//display arguments to the method
+		
+		//get the arguments
+		Object[]args=theJoinPoint.getArgs();
+		
+		//loop thru and display args
+		
+		for(Object tempArg:args) {
+			myLogger.info("====> Argument: "+tempArg);
+		}
+		
+		
+	}
+	
 	// add @AfterReturning advice
+	@AfterReturning(
+			pointcut = "forAppFlow()",
+			returning = "theResult"
+			)
+	public void afterReturningAdvice(JoinPoint theJoinPoint,Object theResult) {
+		
+		//Display method returning from
+		
+		String theMethod=theJoinPoint.getSignature().toShortString();
+		myLogger.info("====> In @AfterReturning: calling method "+theMethod);
+		
+		// display data returned
+		
+		myLogger.info("====> Result: "+theResult);
+		
+	}
 
 }
